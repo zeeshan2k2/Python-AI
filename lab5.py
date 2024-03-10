9b9802c54c39ee07d3012bc75fb1306de8fd2c19
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-03-11 00:33:34 +0500

    completed question 2

---
+++ lab5.py
@@ -0,0 +1,39 @@
+# 1.Program to find minimum and maximum value from a list using in-built functions.
+
+arr1 = [13, 3, 5, 2, 8]
+
+print(f"This is the given array: {arr1}")
+print(f"This is the minimum value: {min(arr1)}")
+print(f"This is the maximum value: {max(arr1)}")
+
+
+# 2.Program to find minimum and maximum value from a list without using in-built functions.
+
+arr2 = [13, 3, 5, 8, 2, 16, 14]
+
+
+maxVal = arr2[0]
+minVal = arr2[1]
+
+
+for i in arr2:
+    if maxVal > i:
+        print("max")
+
+    elif maxVal < i:
+        maxVal = i
+
+for i in arr2:
+    if minVal < i:
+        print("min")
+    elif minVal > i:
+        minVal = i
+    else:
+        print("bruh")
+
+
+
+print(f"This is the given array: {arr2}")
+print(f"This is the maximum value: {maxVal}")
+print(f"This is the minimum value: {minVal}")
+
